94b00a2dbd312166e2167b0842ea55923de67200
John Mark E. Flameno <johnmarkflameno@gmail.com>
2025-04-05 00:00:22 +0800

    Todo-List-Project v1.0.5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		allowedHosts: ['https://to-do-list-project-pc4p.onrender.com'],
+	},
 	plugins: [react(), tailwindcss()],
 });
